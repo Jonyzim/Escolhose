e89807a80c2902c24c0ba114eb83ee23a1061584
Jony33Invent <35571614+Jony33Invent@users.noreply.github.com>
2022-11-11 17:53:29 -0300

    Drag funcionando

--- TesteLapse/Assets/Scenes/Salmaze/Drag.cs
+++ TesteLapse/Assets/Scenes/Salmaze/Drag.cs
@@ -2,15 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class Drag : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     [SerializeField] private RectTransform _transform;
     [SerializeField] private CanvasGroup _canvasGroup;
     Vector3 vetor = new Vector3(0, 0, 0);
-    float armaz;
     [SerializeField] int grau=200;
-    
+    Vector3 initialPos = new Vector3(0, 0, 0);
+    private bool canDrag = true;
+    private bool showingChange = false;
+    private bool goingRight = false;
+    private int state=0;
+    [SerializeField] UnityEvent changeLeft;
+    [SerializeField] UnityEvent changeRight;
+    [SerializeField] UnityEvent resetChange;
+    [SerializeField] UnityEvent chooseRight;
+    [SerializeField] UnityEvent chooseLeft;
+    [SerializeField] float maxX=150f;
+    float exitX = 400f;
+    private void Start()
+    {
+        initialPos = _transform.anchoredPosition;
+    }
     public void OnPointerDown(PointerEventData eventData){
     }
     
@@ -21,20 +36,94 @@ public class Drag : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndD
 
     public void OnDrag(PointerEventData eventData)
     {
-      armaz = _transform.anchoredPosition.x;
-      _transform.anchoredPosition += eventData.delta;
-      vetor.z = tres(900,grau*-1,armaz);
-      //Debug.Log(vetor.z);
-      transform.eulerAngles = vetor;
+        if (canDrag)
+        {
+
+            float _x = _transform.anchoredPosition.x;
+            Vector3 pos = _transform.anchoredPosition;
+            _transform.anchoredPosition += eventData.delta;
+            if (!showingChange)
+            {
+                // right
+                if (_x > 0)
+                {
+                    changeRight.Invoke();
+                    showingChange=true;
+                    goingRight = true;
+                }
+                // left
+                else if (_x < 0)
+                {
+                    changeLeft.Invoke();
+                    showingChange = true;
+                    goingRight = false;
+                }
+            }
+            else
+            {
+                // right
+                if (!goingRight && _x > 0)
+                {
+                    changeRight.Invoke();
+                    goingRight = true;
+                }
+                // left
+                else if (goingRight && _x < 0)
+                {
+                    changeLeft.Invoke();
+                    goingRight = false;
+                }
+            }
+            vetor.z = tres(900, grau * -1, _x);
+            transform.eulerAngles = vetor;
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-      vetor.z = 0;
-      transform.eulerAngles = vetor;       
       _canvasGroup.blocksRaycasts = true;
+        float _x = _transform.anchoredPosition.x;
+        if (_x > maxX)
+        {
+            canDrag = false;
+            StartCoroutine(GoDirection(1,chooseRight));
+        }
+        else if (_x < -maxX)
+        {
+            canDrag = false;
+            StartCoroutine(GoDirection(-1,chooseLeft));
+            
+        }
+        else
+        {
+            ResetPosition();
+        }
+        resetChange.Invoke();
+        showingChange = false;
     }
+    private void ResetPosition()
+    {
 
+        vetor.z = 0;
+        transform.eulerAngles = vetor;
+        _transform.anchoredPosition = initialPos;
+    }
+    [SerializeField] float animSpeed;
+    IEnumerator GoDirection(int dir,UnityEvent e)
+    {
+        Vector3 pos;
+        do
+        {
+             pos = _transform.anchoredPosition;
+            pos.x += animSpeed * dir;
+            _transform.anchoredPosition = pos;
+            yield return new WaitForEndOfFrame();
+        } while (Mathf.Abs(pos.x)< exitX);
+        e.Invoke();
+        ResetPosition();
+        canDrag = true;
+
+    }
     public float tres(float a, float x, float b){
           if(a != 0)
           return (b * x)/a;
