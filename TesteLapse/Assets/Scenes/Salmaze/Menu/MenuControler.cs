570b85f7562896bcbe76d154235845ecc68e3561
KSalmaze <105123464+KSalmaze@users.noreply.github.com>
2022-12-05 01:16:40 -0300

    Sistema simples de volume

---
+++ TesteLapse/Assets/Scenes/Salmaze/Menu/MenuControler.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControler : MonoBehaviour
+{
+    [SerializeField] private GameObject PainelConfig;
+    [SerializeField] AudioSource source;
+    [SerializeField] AudioClip clip;
+    [SerializeField] private string GameScene;
+    public bool sound_fx;
+    public bool music;
+        //[SerializeField] Animator ini;
+    
+    void Start()
+    {
+        source.clip = clip;
+        source.volume = 0.3f;
+       //ini = GetComponent<Animator>();
+    }
+    public void Config()
+    {
+        PainelConfig.SetActive(true);
+       // ini.Play("Go");
+       if (sound_fx == true)
+       {
+           source.Play();
+       }
+    }
+
+    public void StartGame()
+    {
+        //SceneManager.LoadScene(GameScene);
+        Debug.Log("Start Game");
+    }
+
+    public void CloseConfig()
+    { 
+        PainelConfig.SetActive(false);
+        if (sound_fx == true)
+        {
+            source.Play();
+        }
+    }
+
+    public void Music()
+    {
+        music = !music;
+    }
+
+    public void SFX()
+    {
+        sound_fx = !sound_fx;
+    }
+}
